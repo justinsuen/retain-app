f45837a128c3840ceaaa12235e834f51c649effd
Justin Suen <jsuen27@gmail.com>
2017-03-11 14:17:42 -0800

    Add notes component and container

--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     <div class="main-container">
       <app-bar></app-bar>
       <main class="main">
-        content
+        <notes-container></notes-container>
       </main>
     </div>
   `
